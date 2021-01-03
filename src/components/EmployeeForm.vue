f4d804e322cf57b64b521d84790a3416cf370c88
Ajo John <ajo.john@airmeet.com>
2021-01-03 16:01:06 +0530

    initial commit

---
+++ src/components/EmployeeForm.vue
@@ -0,0 +1,37 @@
+<!-- HTML -->
+<template>
+  <div>
+    <h1>Employee Details</h1>
+    <form>
+      <!-- EMPLOYEE NAME -->
+      <label>Name: -</label>
+      <input
+        type="text"
+        name="employeeName"
+        id="employeeName"
+        v-model="employeeName"
+        placeholder="Enter Employee Name"
+      />
+      <!-- END -->
+    </form>
+  </div>
+</template>
+<!-- END -->
+
+<!-- JAVASCRIPT -->
+<script>
+export default {
+  data() {
+    return {
+      employeeName: ""
+    };
+  },
+  methods: {}
+};
+</script>
+<!-- END -->
+
+<!-- CSS -->
+<style scoped>
+</style>
+<!-- END -->
---
+++ src/views/EmployeeDetails.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="EmployeeDetails">
+    <EmployeeForm />
+  </div>
+</template>
+
+<script>
+import EmployeeForm from "@/components/EmployeeForm";
+export default {
+  name: "Employee Details",
+  components: {
+    EmployeeForm
+  }
+};
+</script>
